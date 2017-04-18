1279bd62a7c98ef66a00fcd7adf14c27d0638aac
Nicolas Kagami <nicolaskagami@gmail.com>
2017-04-18 00:04:55 -0300

    Vim colors and lightline

--- vim/build.sh
+++ vim/build.sh
@@ -32,16 +32,17 @@ sudo ln -f -s $VIM_COLOR_FOLDER $VIM_COLOR_FOLDER_LOCATION
 git clone https://github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-curl http://www.vim.org/scripts/download_script.php?src_id=19588 > clang_complete.vmb
-vim clang_complete.vmb -c 'so %' -c 'q'
-rm clang_complete.vmb
+# Install Clang Complete
+#curl http://www.vim.org/scripts/download_script.php?src_id=19588 > clang_complete.vmb
+#vim clang_complete.vmb -c 'so %' -c 'q'
+#rm clang_complete.vmb
 
-
-#cd $VIM_COLOR_FOLDER
-#mkdir tags
-#curl https://vim.sourceforge.io/scripts/download_script.php?src_id=9178 > cpp_src.tar.bz2
-#tar jxf cpp_src.tar.bz2
-#ctags -R --sort=1 --c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++ -f cpp cpp_src
-#ctags -R --sort=yes --c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++ -f sdl /usr/include/SDL2/ # for SDL
-#mv cpp ./tags/
-#mv sdl ./tags/
+# Install Omnicppcomplete
+cd $VIM_COLOR_FOLDER
+mkdir tags
+curl https://vim.sourceforge.io/scripts/download_script.php?src_id=9178 > cpp_src.tar.bz2
+tar jxf cpp_src.tar.bz2
+ctags -R --sort=1 --c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++ -f cpp cpp_src
+ctags -R --sort=yes --c++-kinds=+p --fields=+iaS --extra=+q --language-force=C++ -f sdl /usr/include/SDL2/ # for SDL
+mv cpp ./tags/
+mv sdl ./tags/
